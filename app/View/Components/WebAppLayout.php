d769d1d589915205d6ce4051fb274e968194a033
arvindverma63 <arvindverma630635@gmail.com>
2026-02-25 22:56:53 +0530

    update

--- app/View/Components/WebAppLayout.php
+++ app/View/Components/WebAppLayout.php
@@ -21,6 +21,6 @@ class WebAppLayout extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('layouts.webapp-layout');
+        return view('layouts.weblayout');
     }
 }
